b4ad72c79461c80e1878cd0e307cd0fa9653b481
Kang Aan Wasi <43213343+aanwhs@users.noreply.github.com>
2022-08-16 18:34:15 +0700

    Delete ads directory

--- ads/OnClickAd-1.js
+++
@@ -1,3 +0,0 @@
-function OnClickFunction() {
-	window.open('//luvaihoo.com/afu.php?zoneid=2861509','_blank');
-}
--- ads/OnClickAd-2.js
+++
@@ -1,3 +0,0 @@
-function OnClickFunction() {
-	window.open('//zuphaims.com/afu.php?zoneid=3771802','_blank');
-}
